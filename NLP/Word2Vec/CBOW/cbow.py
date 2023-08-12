ba7035042bcb074c1b571c59c8ac738b4ff006db
AndriievskyiN <n.andrievskiy@gmail.com>
2023-08-12 12:01:41 +0200

    created cbow architecture

---
+++ NLP/Word2Vec/CBOW/cbow.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class CBOW(nn.Module):
+    def __init__(self, vocab_size, d_model, hidden_dim):
+        super(CBOW, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        self.fc1 = nn.Linear(d_model, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, vocab_size)
+
+    def forward(self, x):
+        x = torch.mean(self.embedding(x), dim=1) # (batch_size, seq_len)
+        x = self.fc1(x) # (batch_size, hidden_dim)
+        x = self.fc2(x) # (batch_size, vocab_size)
+        return x
